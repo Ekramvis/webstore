8141aee0dda80bc8224d48a361fb79bd5f753e3e
Angelo+Cody <Angelo+Cody@appacademy.io>
2013-05-13 18:36:45 -0400

    Finished with everything except checkout and orders. Tested everything, all seems to work fine

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,17 +1,31 @@
 class CartsController < ApplicationController
 
-	# create an empty cart for a new user
-	# def create
-	# 	session[:cart] = {}
-	# end
+	before_filter :require_login
 
 	def update
-		params["cart"].each { |key, value| session[:cart][key] += value.to_i }
+		flash[:notice] = []
+		params["cart"].each do |product_id, quant|
+			session[:cart][product_id] += quant.to_i
+			if quant.to_i > 0
+				flash[:notice] << "You've added #{quant} #{Product.find(product_id).name}"
+			end
+		end
 		redirect_to cart_path
 	end
 
 	def show
 		@cart = session[:cart]
+	end
+
+
+	private
+
+	def require_login
+    unless logged_in?
+			flash[:notice] = []
+      flash[:notice] << "You must be logged in to access this section"
+      redirect_to new_session_url
+    end
 	end
 
 end
